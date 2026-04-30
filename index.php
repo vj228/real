0a658387251bf91e28c31786ec4557ac4f2e2ca5
C Yeung <cyeung@MacBook-Pro.local>
2026-04-30 06:25:07 -0700

    text change

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>yHome - Know the real cost before you buy</title>
+    <title>yHome - Know the real cost and risk before you offer</title>
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
